69c42d4aef03d84c1a1296296282b8662f48ce70
pprejs20 <41055520+pprejs20@users.noreply.github.com>
2021-06-09 18:13:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import binascii
+
+
 class parsing_hexdump:
     network_id = -1
     version_max = -1
@@ -12,13 +15,13 @@ class parsing_hexdump:
         self.parse_header()
 
     def parse_header(self):
-        parsing_hexdump.network_id = int(self.working_hexdump[2:4])
-        parsing_hexdump.version_max = int(self.working_hexdump[4:6])
-        parsing_hexdump.version_using = int(self.working_hexdump[6:8])
-        parsing_hexdump.version_min = int(self.working_hexdump[8:10])
-        parsing_hexdump.message_type = int(self.working_hexdump[10:12])
-        parsing_hexdump.extensions.append(int(self.working_hexdump[12:14]))
-        parsing_hexdump.extensions.append(int(self.working_hexdump[14:16]))
+        parsing_hexdump.network_id = self.working_hexdump[1]
+        parsing_hexdump.version_max = self.working_hexdump[2]
+        parsing_hexdump.version_using = self.working_hexdump[3]
+        parsing_hexdump.version_min = self.working_hexdump[4]
+        parsing_hexdump.message_type = self.working_hexdump[5]
+        parsing_hexdump.extensions.append(self.working_hexdump[6])
+        parsing_hexdump.extensions.append(self.working_hexdump[7])
 
 
     def display_header(self):
@@ -30,5 +33,6 @@ class parsing_hexdump:
         print("Extensions: {} {}".format(parsing_hexdump.extensions[0], parsing_hexdump.extensions[1]))
 
 
-h = parsing_hexdump("524212121202000000000000000000000000ffff9df5d11ef0d200000000000000000000ffff18fb4f64f0d200000000000000000000ffff405a48c2f0d200000000000000000000ffff95382eecf0d200000000000000000000ffff2e044970f0d200000000000000000000ffff68cdcd53f0d200000000000000000000ffffb3a2bdeff0d200000000000000000000ffff74ca6b61f0d2")
+input_stream = "524212121202000000000000000000000000ffff9df5d11ef0d200000000000000000000ffff18fb4f64f0d200000000000000000000ffff405a48c2f0d200000000000000000000ffff95382eecf0d200000000000000000000ffff2e044970f0d200000000000000000000ffff68cdcd53f0d200000000000000000000ffffb3a2bdeff0d200000000000000000000ffff74ca6b61f0d2"
+h = parsing_hexdump(binascii.unhexlify(input_stream))
 h.display_header()
