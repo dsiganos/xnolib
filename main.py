f8e8bb41f4572010b5b3c931881103efd361ec1a
pprejs20 <41055520+pprejs20@users.noreply.github.com>
2021-06-10 16:55:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class network_id:
     def __str__(self):
         return chr(self.id)
 
+
 class message_type:
     def __init__(self, data):
         self.parse_type(data)
@@ -66,7 +67,6 @@ class message_header:
         str += "VerMin:%s, "   % self.ver_min
         str += "MsgType:%s, "    % self.msg_type
         str += "Extensions:%s, %s" % (self.ext[0], self.ext[1])
-        # TODO: extensions
         return str
 
 class ipv6addresss:
@@ -84,27 +84,52 @@ class ipv6addresss:
         return str(self.address)
 
 
-class message_body():
-    def __init__(self, data):
-        if len(data) < 18:
-            raise ParseErrorBadMessageBody
-        self.data = data
-        self.parse_peer()
-
-    def parse_peer(self):
-        self.ip = ipv6addresss(self.data[:16])
-        self.port = int.from_bytes(self.data[16:18], "little")
+# A class representing a peer, stores its address, port and provides the means to convert
+# it into a readable string format
+class peer_address:
+    def __init__(self,ip, port):
+        self.ip = ip
+        self.port = port
 
     def __str__(self):
-        string = "Peer: ["
+        string = "["
         string += str(self.ip) + "]:"
         string += str(self.port)
         return string
-    
+
+# Creates, stores and manages all of the peer_address objects (from the raw data)
+class peers():
+    def __init__(self, data):
+        if len(data) % 18 != 0:
+            raise ParseErrorBadMessageBody
+        self.data = data
+        self.peers = []
+        self.parse_peers()
+
+    def parse_peers(self):
+        no_of_peers = int(len(self.data) / 18)
+        start_index = 0
+        end_index = 18
+        for i in range(0, no_of_peers):
+            ip = ipv6addresss(self.data[start_index:end_index-2])
+            port = int.from_bytes(self.data[end_index-2:end_index], "little")
+            p = peer_address(ip, port)
+            self.peers.append(p)
+            start_index = end_index
+            end_index += 18
+
+    def __str__(self):
+        string = ""
+        for i in range(0, len(self.peers)):
+            string += "Peer %d:" % (i+1)
+            string += str(self.peers[i])
+            string += "\n"
+        return string
+
 
 input_stream = "524122222202000000000000000000000000ffff9df5d11ef0d200000000000000000000ffff18fb4f64f0d200000000000000000000ffff405a48c2f0d200000000000000000000ffff95382eecf0d200000000000000000000ffff2e044970f0d200000000000000000000ffff68cdcd53f0d200000000000000000000ffffb3a2bdeff0d200000000000000000000ffff74ca6b61f0d2"
 data = binascii.unhexlify(input_stream)
 h = message_header(data)
-b = message_body(data[8:26])
+b = peers(data[8:])
 print(h)
 print(b)
