39e3038a06b3e8eeb33d97c27af0fb1c0514f5f3
pprejs20 <41055520+pprejs20@users.noreply.github.com>
2021-06-14 15:56:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,13 +28,41 @@ class SocketClosedByPeer(Exception):
     pass
 
 
+class ParseErrorBadBlockSend(Exception):
+    pass
+
+
+class ParseErrorBadBlockReceive(Exception):
+    pass
+
+
+class ParseErrorBadBlockOpen(Exception):
+    pass
+
+
+class ParseErrorBadBlockChange(Exception):
+    pass
+
+
+class ParseErrorBadBlockChange(Exception):
+    pass
+
+
+class ParseErrorBadBlockState(Exception):
+    pass
+
+
+class ParseErrorBadBulkPullResponse(Exception):
+    pass
+
+
 class network_id:
     def __init__(self, rawbyte):
         self.parse_header(int(rawbyte))
 
     def parse_header(self, rawbyte):
-        if not (rawbyte in [ord('A'), ord('B'), ord('C')]):
-            raise ParseErrorBadNetworkId()
+        # if not (rawbyte in [ord('A'), ord('B'), ord('C')]):
+        #     raise ParseErrorBadNetworkId()
         self.id = rawbyte
 
     def __str__(self):
@@ -46,8 +74,8 @@ class message_type:
         self.parse_type(data)
 
     def parse_type(self, data):
-        if not (data in range(2, 13)):
-             raise ParseErrorBadMessageType()
+        # if not (data in range(2, 13)):
+        #      raise ParseErrorBadMessageType()
         self.type = data
 
     def __str__(self):
@@ -79,8 +107,8 @@ class message_header:
     # this need to become a class method
     @classmethod
     def parse_header(cls, data):
-        if data[0] != ord('R'):
-            raise ParseErrorBadMagicNumber()
+        # if data[0] != ord('R'):
+        #     raise ParseErrorBadMagicNumber()
         ext = []
         net_id = network_id(data[1])
         versions = []
@@ -208,25 +236,12 @@ class message_keepmealive:
         if str(self) == str(other):
             return True
         return False
-class message_telemetry_req:
-    def __init__(self):
-        self.header = message_header(network_id(66), [18, 18, 18], message_type(12), [0, 0])
-
-    def serialise(self):
-        return self.header.serialise_header()
-
-    def __str__(self):
-        return str(self.header)
-
-    def __eq__(self, other):
-        if str(self) == str(other):
-            return True
-        return False
 
 class message_bulk_pull:
     def __init__(self):
         self.header = message_header(network_id(66), [18, 18, 18], message_type(6), [0, 0])
         self.public_key = binascii.unhexlify("259A43ABDB779E97452E188BA3EB951B41C961D3318CA6B925380F4D99F0577A")
+
 
     def serialise(self):
         data = self.header.serialise_header()
@@ -234,6 +249,170 @@ class message_bulk_pull:
         data += (0).to_bytes(32, "big")
         return data
 
+class bulk_pull_response:
+    def __init__(self, blocks):
+        self.blocks = blocks
+        self.current_index = 0
+
+    @classmethod
+    def parse_bulk_pull_response(cls, data):
+        current_index = 0
+        prev_index = 0
+        blocks = []
+        while current_index < len(data): # maybe -1 here we'll see
+            prev_index = current_index
+            try:
+                if data[current_index] == 2:
+                    current_index += 153
+                    blocks.append(cls.create_block_send(data[prev_index + 1:current_index]))
+                elif data[current_index] == 3:
+                    current_index += 137
+                    blocks.append(cls.create_block_recv(data[prev_index + 1:current_index]))
+                elif data[current_index] == 4:
+                    current_index += 169
+                    blocks.append(cls.create_block_open(data[prev_index + 1:current_index]))
+                elif data[current_index] == 5:
+                    current_index += 137
+                    blocks.append(cls.create_block_change(data[prev_index + 1:current_index]))
+                elif data[current_index] == 6:
+                    current_index += 217
+                    blocks.append(cls.create_block_state(data[prev_index + 1:current_index]))
+            except IndexError as error:
+                raise ParseErrorBadBulkPullResponse()
+        return bulk_pull_response(blocks)
+
+
+    @classmethod
+    def create_block_send(cls, data):
+        if len(data) != 152:
+            raise ParseErrorBadBlockSend()
+        prev = int.from_bytes(data[:32], "big")
+        dest = int.from_bytes(data[32:64], "big")
+        bal = int.from_bytes(data[64:80], "big")
+        sig = int.from_bytes(data[80:144], "big")
+        work = int.from_bytes(data[144:], "little")
+        return block_send(prev, dest, bal, sig, work)
+
+
+    @classmethod
+    def create_block_recv(cls, data):
+        if len(data) != 136:
+            raise ParseErrorBadBlockReceive()
+        prev = int.from_bytes(data[:32], "big")
+        source = int.from_bytes(data[32:64], "big")
+        sig = int.from_bytes(data[64:128], "big")
+        work = int.from_bytes(data[128:], "little")
+        return block_receive(prev, source, sig, work)
+
+
+    @classmethod
+    def create_block_open(cls, data):
+        if len(data) != 168:
+            raise ParseErrorBadBlockOpen()
+        source = int.from_bytes(data[:32], "big")
+        rep = int.from_bytes(data[32:64], "big")
+        account = int.from_bytes(data[64:96], "big")
+        sig = int.from_bytes(data[96:160], "big")
+        work = int.from_bytes(data[160:], "little")
+        return block_open(source, rep, account, sig, work)
+
+    @classmethod
+    def create_block_change(cls, data):
+        if len(data) != 136:
+            raise ParseErrorBadBlockChange()
+        prev = int.from_bytes(data[:32], "big")
+        rep = int.from_bytes(data[32:64], "big")
+        sig = int.from_bytes(data[64:128], "big")
+        work = int.from_bytes(data[128:], "little")
+        return block_change(prev, rep, sig, work)
+
+
+    @classmethod
+    def create_block_state(cls, data):
+        if len(data) != 216:
+            raise ParseErrorBadBlockState()
+        account = int.from_bytes(data[:32], "big")
+        prev = int.from_bytes(data[32:64], "big")
+        rep = int.from_bytes(data[64:96], "big")
+        bal = int.from_bytes(data[96:112], "big")
+        link = int.from_bytes(data[112:144], "big")
+        sig = int.from_bytes(data[188:208], "big")
+        work = int.from_bytes(data[208:], "big")
+        return block_state(account, prev, rep, bal, link, sig, work)
+
+    def __str__(self):
+        string = "---------------------------------------------------\n"
+        for i in range(0, len(self.blocks)):
+            string += str(self.blocks[i])
+        string += "---------------------------------------------------"
+        return string
+
+
+class block_send:
+    def __init__(self, prev, dest, bal, sig, work):
+        self.prev = prev
+        self.dest = dest
+        self.bal = bal
+        self.sig = sig
+        self.work = work
+
+    def __str__(self):
+        string = "------------- Block Send -------------\n"
+        string += "Previous Node: %d\n" % self.prev
+        string += "Destination Node: %d\n"  % self.dest
+        string += "Balance: %d\n" % self.bal
+        string += "Signature: %d\n" % self.sig
+        string += "Proof of Work: %d" % self.work
+        return string
+
+
+
+class block_receive:
+    def __init__(self, prev, source, sig, work):
+        self.prev = prev
+        self.source = source
+        self.sig = sig
+        self.work = work
+
+class block_open:
+    def __init__(self, source, rep, account, sig, work):
+        self.source = source
+        self.rep = rep
+        self.account = account
+        self.sig = sig
+        self.work = work
+
+    def parse_block_open(self, data):
+        pass
+
+class block_change:
+    def __init__(self, prev, rep, sig, work):
+        self.prev = prev
+        self.rep = rep
+        self.sig = sig
+        self.work = work
+
+class block_state:
+    def __init__(self, account, prev, rep, bal, link, sig, work):
+        print ("making state block")
+        self.account = account
+        self.prev = prev
+        self.rep = rep
+        self.bal = bal
+        self.link = link
+        self.sig = sig
+        self.work = work
+
+    def __str__(self):
+        string = "------------- Block State -------------\n"
+        string += "Account: %s\n" % hex(self.account)
+        string += "Previous: %s\n" % hex(self.prev)
+        string += "Representative: %s\n" % hex(self.rep)
+        string += "Balance: %s\n" % hex(self.bal)
+        string += "Link: %s\n" % hex(self.link)
+        string += "Signature: %s\n" % hex(self.sig)
+        string += "Work: %s\n" % hex(self.work)
+        return string
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("peering-beta.nano.org", 54000))
@@ -252,13 +431,14 @@ def receive_loop(sock):
         # TODO: we expect to get a message header here
         # so ask for 8 bytes, deserialise the 8 bytes as a message header and if it is valid
         # then do work according to the message type
-        data = sock.recv(8)
-        h = message_header.parse_header(data)
-        print(h)
+        data = sock.recv(217)
+        print(binascii.hexlify(data))
+        b = bulk_pull_response.parse_bulk_pull_response(data)
+        print ("printing the bulk pull")
+        print(b)
         break
         if len(data) == 0:
             raise SocketClosedByPeer()
-
 
 
 receive_thread = threading.Thread(target=receive_loop, args=(s,), daemon=True)
