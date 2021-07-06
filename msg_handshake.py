75f9b932a166ed725e7e45956f6eb78f1084cc12
Patryk Prejs <pprejs22@gmail.com>
2021-07-06 13:07:07 +0100

    Updated

--- msg_handshake.py
+++ msg_handshake.py
@@ -11,9 +11,10 @@ s.settimeout(2)
 msg_handshake = message_handshake_query()
 s.send(msg_handshake.serialise())
 data = read_socket(s, 136)
+print(data[6])
 recvd_response = message_handshake_response.parse_msg_handshake_response(data)
+print(recvd_response)
+
 
 response = message_handshake_response.create_handshake_response(recvd_response.cookie)
 s.send(response.serialise())
-data = read_socket(s, 104)
-recvd_response2 = message_handshake_response.parse_msg_handshake_response(data)
