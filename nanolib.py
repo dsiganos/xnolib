54e88639bcafc6b770e1059d3119f8b3bcfd5b3d
Patryk Prejs <pprejs22@gmail.com>
2021-07-06 13:23:45 +0100

    Fixed __str__() in message_handshake_query

--- nanolib.py
+++ nanolib.py
@@ -337,6 +337,7 @@ class message_handshake_query:
     def __str__(self):
         string = "Header: [%s]\n" % str(self.header)
         string += "Cookie: %s" % binascii.hexlify(self.cookie).decode("utf-8").upper()
+        return string
 
 
 class message_handshake_response:
