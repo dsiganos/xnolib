c69afc10e1eb5231a83b2b8b3db68acdd93de9e1
Patryk Prejs <pprejs22@gmail.com>
2021-07-15 15:56:54 +0100

    Some changes and improvements

--- pull_blocks.py
+++ pull_blocks.py
@@ -5,6 +5,7 @@ from nanolib import *
 
 ctx = livectx
 s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
 peeraddr = random.choice(get_all_dns_addresses(ctx['peeraddr']))
 peeraddr = '::ffff:' + peeraddr
 s.settimeout(3)
@@ -13,16 +14,17 @@ print('Connected to [%s]:%s' % (s.getpeername()[0], s.getpeername()[1]))
 
 header = message_header(network_id(67), [18, 18, 18], message_type(6), 0)
 bulk_pull = message_bulk_pull(header, ctx['genesis_pub'])
-# bulk_pull2 = message_bulk_pull('059F68AAB29DE0D3A27443625C7EA9CDDB6517A8B76FE37727EF6A4D76832AD5', network_id(67))
+# bulk_pull2 = message_bulk_pull(header, '42DD308BA91AA225B9DD0EF15A68A8DD49E2940C6277A4BFAC363E1C8BF14279')
 req = bulk_pull.serialise()
 s.send(req)
 # req = bulk_pull2.serialise()
-# s.send(req)
 
 blocks = read_blocks_from_socket(s)
 
 manager = blocks_manager()
 while len(blocks) != 0:
-    manager.process(blocks.pop())
+    block = blocks.pop()
+    manager.process(block)
 
-print(manager.accounts[0].str_blocks())
+print(manager)
+# print(manager.str_processed_blocks())
