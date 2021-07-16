9063f637cbc7388eb53db9a949bd8c8823d12a67
Patryk Prejs <pprejs22@gmail.com>
2021-07-16 13:34:52 +0100

    Channged script to work with the new get_account_blocks() function

--- pull_blocks.py
+++ pull_blocks.py
@@ -12,14 +12,7 @@ s.settimeout(3)
 s.connect((peeraddr, ctx['peerport']))
 print('Connected to [%s]:%s' % (s.getpeername()[0], s.getpeername()[1]))
 
-header = message_header(network_id(67), [18, 18, 18], message_type(6), 0)
-bulk_pull = message_bulk_pull(header, ctx['genesis_pub'])
-# bulk_pull = message_bulk_pull(header, '42DD308BA91AA225B9DD0EF15A68A8DD49E2940C6277A4BFAC363E1C8BF14279')
-req = bulk_pull.serialise()
-s.send(req)
-# req = bulk_pull2.serialise()
-
-blocks = read_blocks_from_socket(s)
+blocks = get_account_blocks(s, ctx["genesis_pub"])
 
 manager = blocks_manager()
 while len(blocks) != 0:
@@ -27,4 +20,3 @@ while len(blocks) != 0:
     manager.process(block)
 
 print(manager)
-# print(manager.str_processed_blocks())
