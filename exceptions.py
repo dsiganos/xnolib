153f98f4b10a9c767f1539e5250f4358acb57a56
Dimitrios Siganos <dimitris@siganos.org>
2021-07-19 14:12:41 +0100

    Move exceptions to their own file

---
+++ exceptions.py
@@ -0,0 +1,52 @@
+class ParseErrorBadMagicNumber(Exception): pass
+
+
+class ParseErrorBadNetworkId(Exception): pass
+
+
+class ParseErrorBadMessageType(Exception): pass
+
+
+class ParseErrorBadIPv6(Exception): pass
+
+
+class ParseErrorBadMessageBody(Exception): pass
+
+
+class ParseErrorBadBlockSend(Exception): pass
+
+
+class ParseErrorBadBlockReceive(Exception): pass
+
+
+class ParseErrorBadBlockOpen(Exception): pass
+
+
+class ParseErrorBadBlockChange(Exception): pass
+
+
+class ParseErrorBadBlockChange(Exception): pass
+
+
+class ParseErrorBadBlockState(Exception): pass
+
+
+class ParseErrorBadBulkPullResponse(Exception): pass
+
+
+class BadBlockHash(Exception): pass
+
+
+class SocketClosedByPeer(Exception): pass
+
+
+class InvalidBlockHash(Exception): pass
+
+
+class HandshakeExchangeFail(Exception): pass
+
+
+class CommsError(Exception): pass
+
+
+
--- nanolib.py
+++ nanolib.py
@@ -10,57 +10,7 @@ import ed25519_blake2
 import ed25519_blake2b
 import git
 
-
-class ParseErrorBadMagicNumber(Exception): pass
-
-
-class ParseErrorBadNetworkId(Exception): pass
-
-
-class ParseErrorBadMessageType(Exception): pass
-
-
-class ParseErrorBadIPv6(Exception): pass
-
-
-class ParseErrorBadMessageBody(Exception): pass
-
-
-class ParseErrorBadBlockSend(Exception): pass
-
-
-class ParseErrorBadBlockReceive(Exception): pass
-
-
-class ParseErrorBadBlockOpen(Exception): pass
-
-
-class ParseErrorBadBlockChange(Exception): pass
-
-
-class ParseErrorBadBlockChange(Exception): pass
-
-
-class ParseErrorBadBlockState(Exception): pass
-
-
-class ParseErrorBadBulkPullResponse(Exception): pass
-
-
-class BadBlockHash(Exception): pass
-
-
-class SocketClosedByPeer(Exception): pass
-
-
-class InvalidBlockHash(Exception): pass
-
-
-class HandshakeExchangeFail(Exception): pass
-
-
-class CommsError(Exception): pass
-
+from exceptions import *
 
 def writefile(filename, content):
     with open(filename, "w") as f:
