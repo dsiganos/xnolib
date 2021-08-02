50458153261b0c6651f47e92f37433fe9ed07b13
Dimitrios Siganos <dimitris@siganos.org>
2021-08-02 15:35:20 +0100

    Bugfix remove required from -t/-b argparse group

--- peercrawler.py
+++ peercrawler.py
@@ -115,7 +115,7 @@ class peer_manager:
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    group = parser.add_mutually_exclusive_group(required=True)
+    group = parser.add_mutually_exclusive_group()
     group.add_argument('-b', '--beta', action='store_true', default=False,
                        help='use beta network')
     group.add_argument('-t', '--test', action='store_true', default=False,
