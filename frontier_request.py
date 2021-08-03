d844e1adee7b16f676f98ae359ea0ac449f3c912
Dimitrios Siganos <dimitris@siganos.org>
2021-08-03 20:00:43 +0100

    feature: frontier request script to contacts all peers

--- frontier_request.py
+++ frontier_request.py
@@ -5,6 +5,7 @@ import random
 import socket
 import argparse
 import lmdb
+import peercrawler
 from exceptions import *
 
 from pynanocoin import *
@@ -60,6 +61,9 @@ def parse_args():
     group.add_argument('-t', '--test', action='store_true', default=False,
                        help='use test network')
 
+    parser.add_argument('-a', '--all', action='store_true', default=False,
+                        help='Download frontiers from all peers')
+
     parser.add_argument('-c', '--count', type=int, default=0xffffffff,
                         help='number of frontiers to download, if not set, all frontiers are downloaded')
     parser.add_argument('-m', '--maxage', type=int, default=0xffffffff,
@@ -68,10 +72,11 @@ def parse_args():
                         help='also download not confirmed blocks')
     parser.add_argument('-s', '--start_acc', default='00'*32,
                         help='start account')
+
     parser.add_argument('-p', '--peer',
                         help='peer to contact for frontiers (if not set, one is randomly selected using DNS)')
     parser.add_argument('--db',
-                        help='save frontiers in the database named by the argument')
+                        help='save frontiers in databases in the folder named by the argument')
     return parser.parse_args()
 
 
@@ -95,6 +100,29 @@ def frontier_to_db(tx, counter, frontier):
     tx.put(frontier.account, frontier.frontier_hash)
 
 
+def get_frontiers_from_peer(peer, frontier_req, use_db):
+    assert isinstance(frontier_req, frontier_request)
+
+    with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+        s.settimeout(3)
+        print('connecting to %s', peer)
+        s.connect((str(peer.ip), peer.port))
+
+        #peer = ip_addr.from_string(peerstr)
+        if use_db:
+            os.makedirs(use_db, exist_ok=True)
+            filename = '%s/%s:%s' % (use_db, str(peer.ip), str(peer.port))
+            lmdb_env = lmdb.open(filename, subdir=False, max_dbs=10000, map_size=10*1000*1000*1000)
+            with lmdb_env.begin(write=True) as tx:
+                s.send(frontier_req.serialise())
+                read_all_frontiers(s, lambda c, f: tx.put(f.account, f.frontier_hash))
+            lmdb_env.close()
+        else:
+            s.send(frontier_req.serialise())
+            read_all_frontiers(s, print_handler)
+
+
 def main():
     args = parse_args()
 
@@ -104,29 +132,36 @@ def main():
 
     confirmed = not args.notconfirmed
 
-    if args.peer:
-        peerstr = str(ip_addr.from_string(args.peer))
-        s, peer = get_initial_connected_socket(ctx, [peerstr])
+    frontier_req = frontier_request(ctx = ctx,
+                                    start_account = binascii.unhexlify(args.start_acc),
+                                    maxage = args.maxage,
+                                    maxacc = args.count,
+                                    confirmed = confirmed)
+
+    if args.all:
+        _, peers = peercrawler.get_peers_from_service()
+        assert peers
     else:
-        s, peer = get_initial_connected_socket(ctx)
-    assert s
+        if args.peer:
+            peers = [peer(ip_addr(ipaddress.IPv6Address(args.peer)), ctx['peerport'])]
+        else:
+            peer = random.choice(get_all_dns_addresses(ctx['peeraddr']))
+            peers = [peer(ip_addr(ipaddress.IPv6Address(peer)), ctx['peerport'])]
 
-    s.settimeout(60)
+    for peer in peers:
+        if peer.score <= 0:
+            continue
 
-    frontier = frontier_request(ctx = ctx,
-                                start_account = binascii.unhexlify(args.start_acc),
-                                maxage = args.maxage,
-                                maxacc = args.count,
-                                confirmed = confirmed)
-    s.send(frontier.serialise())
-
-    if args.db:
-        lmdb_env = lmdb.open(args.db, map_size=10*1000*1000*1000)
-        with lmdb_env.begin(write=True) as tx:
-            read_all_frontiers(s, lambda c, f: tx.put(f.account, f.frontier_hash))
-        lmdb_env.close()
-    else:
-        read_all_frontiers(s, print_handler)
+        try:
+            get_frontiers_from_peer(peer, frontier_req, args.db)
+        except OSError as e:
+            # peer was connectable but some other error happpened, score it with 1
+            peer.deduct_score(200)
+            print('Exception %s: %s' % (type(e), e))
+        except PyNanoCoinException as e:
+            # peer was connectable but some other error happpened, score it with 1
+            peer.deduct_score(200)
+            print('Exception %s: %s' % (type(e), e))
 
 
 if __name__ == "__main__":
