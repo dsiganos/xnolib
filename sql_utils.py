303581acf84f042f8ad998250e7d1f0d7c7aa32e
Dimitrios Siganos <dimitris@siganos.org>
2021-08-03 12:17:21 +0100

    Bugfix capitalisation problem with table Peer

--- sql_utils.py
+++ sql_utils.py
@@ -18,7 +18,7 @@ def create_db_structure_frontier_service(cursor):
     cursor.execute("CREATE TABLE Peers (peer_id VARCHAR(36) PRIMARY KEY, ip_address VARCHAR(50), port int, score int)")
     cursor.execute("CREATE TABLE Frontiers (peer_id VARCHAR(36), frontier_hash VARCHAR(64), " +
                    "account_hash VARCHAR(64), PRIMARY KEY(peer_id, account_hash), " +
-                   "FOREIGN KEY (peer_id) REFERENCES peers(peer_id))")
+                   "FOREIGN KEY (peer_id) REFERENCES Peers(peer_id))")
 
 
 def query_accounts_different_hashes(cursor):
