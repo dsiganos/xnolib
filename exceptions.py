3ded44a2f16644bec802ddf951b69adb3b8a155b
Patryk Prejs <pprejs22@gmail.com>
2021-08-05 18:29:39 +0100

    Added BlacklistItemTypeError exception

--- exceptions.py
+++ exceptions.py
@@ -82,3 +82,7 @@ class PeerServiceUnavailable(PyNanoCoinException):
 
 class FrontierServiceSlowPeer(PyNanoCoinException):
     pass
+
+
+class BlacklistItemTypeError(PyNanoCoinException):
+    pass
