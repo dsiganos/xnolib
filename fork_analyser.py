f4c604a701de9c45af84b9d92b9faeb31c1d517b
Dimitrios Siganos <dimitris@siganos.org>
2021-08-08 19:18:25 +0100

    fork analyser: analyse all known forked accounts

--- fork_analyser.py
+++ fork_analyser.py
@@ -94,10 +94,13 @@ def once(ctx, workdir, forkacc):
     print('Pulled blocks from %s out of %s peers' % (pulls, len(peers)))
 
 
-ctx=livectx
+ctx = livectx
 
-fork1 = binascii.unhexlify('7D6FE3ABD8E2F7598911E13DC9C5CD2E71210C1FBD90D503C7A2041FBF58EEFD')
-fork2 = binascii.unhexlify('CC83DA473B2B1BA277F64359197D4A36866CC84A7D43B1F65457324497C75F75')
+# nano_3m65ub5mpcrunbuzeits57ynnfn8fm66nzc5p9u7aoskakdwgquoiszd4zz1
+fork1 = binascii.unhexlify('cc83da473b2b1ba277f64359197d4a36866cc84a7d43b1f65457324497c75f75')
+
+# nano_1zdhwgoxjrqqd86j5rbxs94wtdmj6683zheitn3whai65yzojuqxyfxzmzj8
+fork2 = binascii.unhexlify('7d6fe3abd8e2f7598911e13dc9c5cd2e71210c1fbd90d503c7a2041fbf58eefd')
 
 # nano_35u3s789f3to4956beqm6t3tnt99oiqxtu4juxezrtsnua1k7nxhnwexrt8d
 fork3 = binascii.unhexlify('8F61C94C76875511C644B2F32683AA68E7AC2FDD6C51DF59FC6B34DA0122D3AF')
@@ -108,12 +111,49 @@ fork4 = binascii.unhexlify('82567f7f268774aa1fcbf821edc045809e1236a8dacf190c8b65
 # nano_1gwu55iga8pf3bpef3shfqgmdgqj3xsqpptuqqnknb4zu4f8cxcyk7ezd36p
 fork5 = binascii.unhexlify('3b9b18e0e41acd0a6cc6872f6ddd35baf10f737b5b5bbde92a245fd89a65755e')
 
-acc_ids = [
-    livectx["genesis_pub"],
-    '42DD308BA91AA225B9DD0EF15A68A8DD49E2940C6277A4BFAC363E1C8BF14279',
-#    '3309D2BDB2DCE1C5744F357E39DC8AC85980F00499F8F43B0A1287D0658C7173',
+# nano_3dzw98zso4gk9b19jho1ao5ie18bzhxrrdpzbf1eoiyeagudk35w176ybx48
+fork6 = binascii.unhexlify('affc39bf9a89d23a4078bea045470600c9fbfb8c2edf4b40cac3cc43b6b9047c')
+
+# nano_1jkexaqe9yxcd4wr1sy5p4hndpbz9dquouwrcd5oauuqymc1k6soacpww3ry
+fork7 = binascii.unhexlify('464cea2ec3fbaa58b98067c3b09f45d93f3aefbaef9852c7546f77f4d4091335')
+
+# nano_35gp6d11p4uw4cyu9xgoths3xu1dpophfixaxy56jtr971rwz9ngtf715add
+fork8 = binascii.unhexlify('8dd622c00b0b7c12bdb3f5d5d3f21eec0bb56cf6c3a8ef8648eb072831cf9e8e')
+
+# nano_1krrw3w3xfzeiex7463nmmmst9ycgabmg5k9bsuxokt3pdm4fu8abj478t9y
+fork9 = binascii.unhexlify('4b18e0781eb7ec833a5110349ce79d1fca7213370e474e77dacb41b2e626ecc8')
+
+# nano_34pdjoy6c7e8foj3srbppszqa9j3kun79yb8mxfjsfmr978g83cmjmgpff59
+fork10 = binascii.unhexlify('8acb8d7c4515866d621ce136b67f741e2196e853f9269f5b1cb678394ce30553')
+
+# nano_3h4kaw6hg6oc5ze4ak5btpgq5adeyfu7tzc7jfdpifxt4ftpsg7pm6pjwtug
+fork11 = binascii.unhexlify('bc524708f712aa1fd8244869d59d71a16cf3765d7d458b576837ba13756cb8b6')
+
+# nano_1z9myhyffmfikpjk6qh5sbx7n8hifcm95it8q3898cmdktmpxgko16sy3pfo
+fork12 = binascii.unhexlify('7cf3f3fcd6cdb095a3225de3ca7a5a19f06aa671c346b84c732a6b96a76eba55')
+
+# nano_3h5n3hpud9hho74ybcibqpuwdkbbzzbhb8z6osbbq8rayik5eciw1o83cgh8
+fork13 = binascii.unhexlify('bc740bedb59defa945e4aa09bdb7c5c929ffd2f49be4ae529b9b08f424362a1c')
+
+# nano_3swpttz8t86zywz7xa83wb9ygsq89y71i7eyg9ackeix1nubzng9uj7aw9ha
+fork14 = binascii.unhexlify('e796d6be6d189ff73e5ea0c1e24fe766e63f8a08159e71d0a9321d05369fd1c7')
+
+
+forks = [
     fork1,
     fork2,
+    fork3,
+    fork4,
+    fork5,
+    fork6,
+    fork7,
+    fork8,
+    fork9,
+    fork10,
+    fork11,
+    fork12,
+    fork13,
+    fork14,
 ]
 
 topworkdir = 'fork_analyser.data'
@@ -121,5 +161,5 @@ os.makedirs(topworkdir, exist_ok=True)
 workdir = tempfile.mkdtemp(dir=topworkdir)
 print(workdir)
 
-once(ctx, workdir, fork1)
-once(ctx, workdir, fork2)
+for fork in forks:
+    once(ctx, workdir, fork)
