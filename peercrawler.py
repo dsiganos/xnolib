ad68e6ee24f2af0641fd6e8c0052e30c96de9772
Dimitrios Siganos <dimitris@siganos.org>
2021-08-12 09:41:42 +0100

    peercrawler client: use ::ffff:46.101.61.203 by default (it was ::1)

--- peercrawler.py
+++ peercrawler.py
@@ -255,7 +255,7 @@ def run_peer_service_forever(peerman, addr='', port=7070):
         conn.close()
 
 
-def get_peers_from_service(ctx, addr = '::1'):
+def get_peers_from_service(ctx, addr = '::ffff:46.101.61.203'):
     s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
     s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
     s.settimeout(5)
