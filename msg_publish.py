b43dd15533728fa10ba62fb5bf2d7fd301dba8b5
Patryk Prejs <pprejs22@gmail.com>
2021-08-18 20:33:41 +0100

    msg_publish main script now in the main() function

--- msg_publish.py
+++ msg_publish.py
@@ -12,20 +12,25 @@ class msg_publish:
         return data
 
 
-header = message_header(network_id(66), [18, 18, 18], message_type(3), 0)
+def main():
+    header = message_header(network_id(66), [18, 18, 18], message_type(3), 0)
 
-example_block_send = {
-    "prev" : binascii.unhexlify('4A039AD482C917C266A3D4A2C97849CE69173B6BC775AFC779B9EA5CE446426F'),
-    "dest" : binascii.unhexlify('42DD308BA91AA225B9DD0EF15A68A8DD49E2940C6277A4BFAC363E1C8BF14279'),
-    "bal" : 100,
-    "sig" : binascii.unhexlify('30A5850305AA61185008D4A732AA8527682D239D85457368B6A581F517D5F8C0078DB99B5741B79CC29880387292B64F668C964BE1B50790D3EC7D948396D007'),
-    "work" : binascii.unhexlify('EDFD7157025EA461')
-}
-block = block_send(example_block_send["prev"], example_block_send["dest"], example_block_send["bal"],
-                   example_block_send["sig"], example_block_send["work"])
-msg = msg_publish(header, block)
+    example_block_send = {
+        "prev" : binascii.unhexlify('4A039AD482C917C266A3D4A2C97849CE69173B6BC775AFC779B9EA5CE446426F'),
+        "dest" : binascii.unhexlify('42DD308BA91AA225B9DD0EF15A68A8DD49E2940C6277A4BFAC363E1C8BF14279'),
+        "bal" : 100,
+        "sig" : binascii.unhexlify('30A5850305AA61185008D4A732AA8527682D239D85457368B6A581F517D5F8C0078DB99B5741B79CC29880387292B64F668C964BE1B50790D3EC7D948396D007'),
+        "work" : binascii.unhexlify('EDFD7157025EA461')
+    }
+    block = block_send(example_block_send["prev"], example_block_send["dest"], example_block_send["bal"],
+                       example_block_send["sig"], example_block_send["work"])
+    msg = msg_publish(header, block)
 
-s, _ = get_initial_connected_socket(betactx)
-with s:
-    s.send(msg.serialise())
-    print(s.recv(1000))
+    s, _ = get_initial_connected_socket(betactx)
+    with s:
+        s.send(msg.serialise())
+        print(s.recv(1000))
+
+
+if __name__ == '__main__':
+    main()
