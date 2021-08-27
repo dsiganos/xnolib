92cb5caec7e1d48f10d2c9398e5aeabc59b0396b
Patryk Prejs <pprejs22@gmail.com>
2021-08-27 17:30:39 +0100

    Updated script, added thread_manager

--- representative_script.py
+++ representative_script.py
@@ -9,6 +9,59 @@ import threading
 import random
 
 
+class enum_error_type:
+    NoError = 0
+    SocketError = 1
+    AccountError = 2
+
+
+class thread_manager:
+    def __init__(self, ctx, peers, representatives):
+        self.ctx = ctx
+        self.peers = peers
+        self.representatives = representatives
+        self.peers_in_use = []
+        self.threads = []
+        self.mutex = threading.Lock()
+
+    def update_manager(self):
+        for data in self.threads:
+            peer = data[0]
+            result = data[1]
+            account = data[2]
+            thread = data[3]
+            if not thread.is_alive():
+                self.threads.remove(data)
+                self.peers_in_use.remove(peer)
+                if result == enum_error_type.NoError:
+                    continue
+                elif result == enum_error_type.AccountError:
+                    # this may change depending on what we decide to do with accounts that yield no blocks
+                    continue
+                elif result == enum_error_type.SocketError:
+                    self.get_account_reps(account)
+
+    def get_account_reps(self, account):
+        while len(self.threads) == 30:
+            self.update_manager()
+        peer = random.choice(self.peers)
+        while peer in self.peers_in_use:
+            self.update_manager()
+            peer = random.choice(self.peers)
+        result = None
+
+        thread = threading.Thread(target=get_representative_thread,
+                                  args=(self.ctx, account, self.representatives, self.mutex, peer, result,),
+                                  daemon=True)
+        self.threads.append((peer, result, account, thread))
+        thread.start()
+        self.peers_in_use.append(peer)
+
+    def all_threads_finished(self):
+        self.update_manager()
+        return True if len(self.threads) == 0 else False
+
+
 def find_most_recent_block_type(blocks, block_type):
     for i in range(0, len(blocks)):
         if isinstance(blocks[i], block_type):
@@ -33,27 +86,27 @@ def remove_finished_threads(threads):
             threads.remove(t)
 
 
-def get_representative_thread(ctx, acc, representatives, mutex, peers):
+def get_representative_thread(ctx, acc, representatives, mutex, peer, result):
     counter = 1
     s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
     with s:
         s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
-        while True:
-            try:
-                peer = random.choice(peers)
-                s.connect((str(peer.ip), peer.port))
-                s.settimeout(3)
-                blocks = get_account_blocks(ctx, s, acc, no_of_blocks=counter)
-                block = blocks[0]
-                break
 
-            # Sometimes there are no blocks read from the socket
-            except IndexError as e:
-                return
+        try:
+            s.connect((str(peer.ip), peer.port))
+            s.settimeout(3)
+            blocks = get_account_blocks(ctx, s, acc, no_of_blocks=counter)
+            block = blocks[0]
 
-            # Socket sometimes times out or doesn't connect
-            except (socket.error, OSError) as e:
-                continue
+        # Sometimes there are no blocks read from the socket
+        except IndexError as e:
+            result = enum_error_type.AccountError
+            return
+
+        # Socket sometimes times out or doesn't connect
+        except (socket.error, OSError) as e:
+            result = enum_error_type.SocketError
+            return
 
         finished = False
 
@@ -63,6 +116,7 @@ def get_representative_thread(ctx, acc, representatives, mutex, peers):
             if type(block) in [block_open, block_state, block_change]:
                 with mutex:
                     representatives.add(block.representative)
+                    result = enum_error_type.NoError
                 finished = True
                 continue
 
@@ -100,22 +154,16 @@ def main():
         representatives = set()
 
         starttime2 = time.time()
-        threads = []
+        threadman = thread_manager(ctx, peers, representatives)
+
         # Go through each account
         for front in frontiers:
-
             acc = front.account
-            mutex = threading.Lock()
-            thread = threading.Thread(target=get_representative_thread,
-                                      args=(ctx, acc, representatives, mutex, peers,),
-                                      daemon=True)
-            while len(threads) == 30:
-                remove_finished_threads(threads)
-            thread.start()
-            threads.append(thread)
+            threadman.get_account_reps(acc)
 
-    for t in threads:
-        t.join()
+    while not threadman.all_threads_finished():
+        pass
+
     endtime = time.time()
     timetaken1 = endtime - starttime1
     timetaken2 = endtime - starttime2
