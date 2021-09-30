2f968b9cb543b3ad3a412cba9e6432f637abf60c
Dimitrios Siganos <dimitris@siganos.org>
2021-10-01 00:49:01 +0100

    dbtools/peers.py: do delete whilst iterating

--- dbtools/peers.py
+++ dbtools/peers.py
@@ -28,9 +28,12 @@ def delete_peers(filename):
     with lmdb.open(filename, subdir=False, max_dbs=10000, map_size=10*1000*1000*1000) as env:
         peers_db = env.open_db(b'peers')
         with env.begin(write=True) as tx:
+            peers = []
             for key, value in tx.cursor(db=peers_db):
+                peers.append(key)
+            for peer in peers:
                 print('Deleting peer %s' % parse_entry(key))
-                tx.delete(key, db=peers_db)
+                tx.delete(peer, db=peers_db)
 
 
 def parse_args():
