292a3891b12b6fa3eb189b34366582e5925c2f13
Dimitrios Siganos <dimitris@siganos.org>
2021-11-17 21:17:51 +0000

    Script to verify POW and signature of a block

---
+++ verify_block.py
@@ -0,0 +1,25 @@
+#!/bin/env python3
+
+# Read a block from stdin and validate its signature and proof of work.
+# Currently only json format is supported
+
+import sys
+
+import block
+import pynanocoin
+
+def to_pass_fail(status):
+    return 'PASS' if status == True else 'FAIL'
+
+json_block = sys.stdin.read()
+print('Input Data:')
+print(json_block)
+
+blk = block.Block.parse_from_json_string(json_block)
+print(blk)
+
+pow_status = pynanocoin.verify_pow(blk)
+sig_status = pynanocoin.valid_block(pynanocoin.livectx, blk)
+
+print('POW       check: %s' % to_pass_fail(pow_status))
+print('Signature check: %s' % to_pass_fail(sig_status))
