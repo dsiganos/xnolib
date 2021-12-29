8c950e93a99afd429069aba34ab640508e7b1707
Dimitrios Siganos <dimitris@siganos.org>
2021-12-29 02:26:51 +0200

    peers_table.py: ability to delete peers

--- peers_table.py
+++ peers_table.py
@@ -48,6 +48,27 @@ class PeersTable:
                     assert rc
 
 
+    def delete_peer(self, peer_str):
+        assert peer_str is not None
+        ipaddr_str, port = parse_endpoint(peer_str, default_port=7075)
+        data = ip_addr(ipaddr_str).serialise() + port.to_bytes(2, "big")
+        assert len(data) == 18
+
+        with lmdb.open(self.filename, subdir=False, max_dbs=10000, map_size=10*1000*1000*1000) as env:
+            peers_db = env.open_db(b'peers')
+            with env.begin(write=True) as tx:
+                peer = None
+                for key, value in tx.cursor(db=peers_db):
+                    if key == data:
+                        peer = key
+                        break
+                if peer is not None:
+                    print('Deleting peer %s' % PeersTable.parse_entry(peer))
+                    tx.delete(peer, db=peers_db)
+                else:
+                    print('Cannot find peer %s to delete' % PeersTable.parse_entry(data))
+
+
     def parse_entry(data):
         assert len(data) == 18
         ipv6 = ipaddress.IPv6Address(data[:-2])
@@ -80,6 +101,8 @@ def main():
         peers_table.delete_peers()
     elif args.command == 'add':
         peers_table.add_peer(args.peer)
+    elif args.command == 'delete':
+        peers_table.delete_peer(args.peer)
     else:
         print('Unknown command %s', args.command)
 
