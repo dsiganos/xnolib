da04324c401f11ccb6a84b77f771f202f0968b1b
Dimitrios Siganos <dimitris@siganos.org>
2021-12-29 02:12:45 +0200

    peers_table.py: add ability to add peers to the database

--- peers_table.py
+++ peers_table.py
@@ -5,41 +5,65 @@ import binascii
 import ipaddress
 import argparse
 
-
-def parse_entry(data):
-    assert len(data) == 18
-    ipv6 = ipaddress.IPv6Address(data[:-2])
-    port = int.from_bytes(data[-2:], "big")
-    ipstr = str(ipv6)
-    if ipv6.ipv4_mapped:
-        ipstr = '::ffff:' + str(ipv6.ipv4_mapped)
-    return '[%s]:%s' % (ipstr, port)
+from pynanocoin import parse_endpoint, ip_addr
 
 
-def print_peers(filename):
-    with lmdb.open(filename, subdir=False, max_dbs=10000, map_size=10*1000*1000*1000) as env:
-        peers_db = env.open_db(b'peers')
-        with env.begin() as tx:
-            for key, value in tx.cursor(db=peers_db):
-                print(parse_entry(key))
+class PeersTable:
+    def __init__(self, filename):
+        self.filename = filename
 
 
-def delete_peers(filename):
-    with lmdb.open(filename, subdir=False, max_dbs=10000, map_size=10*1000*1000*1000) as env:
-        peers_db = env.open_db(b'peers')
-        with env.begin(write=True) as tx:
-            peers = []
-            for key, value in tx.cursor(db=peers_db):
-                peers.append(key)
-            for peer in peers:
-                print('Deleting peer %s' % parse_entry(key))
-                tx.delete(peer, db=peers_db)
+    def print_peers(self):
+        with lmdb.open(self.filename, subdir=False, max_dbs=10000, map_size=10*1000*1000*1000) as env:
+            peers_db = env.open_db(b'peers')
+            with env.begin() as tx:
+                for key, value in tx.cursor(db=peers_db):
+                    print(PeersTable.parse_entry(key))
+
+
+    def delete_peers(self):
+        with lmdb.open(self.filename, subdir=False, max_dbs=10000, map_size=10*1000*1000*1000) as env:
+            peers_db = env.open_db(b'peers')
+            with env.begin(write=True) as tx:
+                peers = []
+                for key, value in tx.cursor(db=peers_db):
+                    peers.append(key)
+                for peer in peers:
+                    print('Deleting peer %s' % PeersTable.parse_entry(key))
+                    tx.delete(peer, db=peers_db)
+
+
+    def add_peer(self, peer_str):
+        assert peer_str is not None
+        ipaddr_str, port = parse_endpoint(peer_str, default_port=7075)
+        data = ip_addr(ipaddr_str).serialise() + port.to_bytes(2, "big")
+        assert len(data) == 18
+
+        with lmdb.open(self.filename, subdir=False, max_dbs=10000, map_size=10*1000*1000*1000) as env:
+            peers_db = env.open_db(b'peers')
+            print('Adding peer [%s]:%s' % (ipaddr_str, port))
+            with env.begin(write=True) as tx:
+                with tx.cursor(db=peers_db) as curs:
+                    rc = curs.put(data, b'')
+                    assert rc
+
+
+    def parse_entry(data):
+        assert len(data) == 18
+        ipv6 = ipaddress.IPv6Address(data[:-2])
+        port = int.from_bytes(data[-2:], "big")
+        ipstr = str(ipv6)
+        if ipv6.ipv4_mapped:
+            ipstr = '::ffff:' + str(ipv6.ipv4_mapped)
+        return '[%s]:%s' % (ipstr, port)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dataldb', default='data.ldb',
                         help='data.ldb path')
+    parser.add_argument('-p', '--peer',
+                        help='peer to add or delete')
     parser.add_argument('command',
             help='command: print or delete')
     return parser.parse_args()
@@ -47,12 +71,17 @@ def parse_args():
 
 def main():
     args = parse_args()
+
+    peers_table = PeersTable(args.dataldb);
+
     if args.command == 'print':
-        print_peers(args.dataldb)
-    elif args.command == 'delete':
-        delete_peers(args.dataldb)
+        peers_table.print_peers()
+    elif args.command == 'delall':
+        peers_table.delete_peers()
+    elif args.command == 'add':
+        peers_table.add_peer(args.peer)
     else:
-        print('Uknown command %s', args.command)
+        print('Unknown command %s', args.command)
 
 
 if __name__ == "__main__":
