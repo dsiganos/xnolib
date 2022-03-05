775f604a74aa0ffa0a4991a9a6778836e0f0ea35
Dimitrios Siganos <dimitris@siganos.org>
2022-03-05 21:59:01 +0000

    Script showing how to POW calculations

---
+++ examples/pow_example.py
@@ -0,0 +1,53 @@
+#!/bin/env python3
+
+import hashlib
+
+epoch1_threshold = 0xffffffc000000000
+epoch2_threshold_high = 0xfffffff800000000
+epoch2_threshold_low  = 0xfffffe0000000000
+
+
+# take a nonce (8 byte unsigned int) and a root (32 byte unsigned int) and produce
+# blake2b(nonce + root), which is a hash of the nonce and the root
+# the output hash is an 8 byte unsigned integer
+def generate_pow_hash(nonce, root):
+    alg = hashlib.blake2b(digest_size=8)
+    alg.update(nonce.to_bytes(8, byteorder='little'))
+    alg.update(root.to_bytes(32, byteorder='big'))
+    return int.from_bytes(alg.digest(), byteorder='little')
+
+
+#
+# check genesis block
+#
+
+genesis_root     = 0xE89208DD038FBB269987689621D52292AE9C35941A7484756ECCED92A65093BA
+genesis_pow      = 0x62F05417DD3FB691
+digest = generate_pow_hash(genesis_pow, genesis_root)
+print(hex(digest))
+print(hex(epoch1_threshold))
+print(digest >= epoch1_threshold)
+
+
+#
+# check first epoch block of genesis account
+#
+
+root = 0xECCB8CB65CD3106EDA8CE9AA893FEAD497A91BCA903890CBD7A5C59F06AB9113
+pow1 = 0x0F78168D5B30191D
+digest = generate_pow_hash(pow1, root)
+print(hex(digest))
+print(hex(epoch1_threshold))
+print(digest >= epoch1_threshold)
+
+
+#
+# check second epoch block of genesis account
+#
+
+root = 0x6875C0DBFE5C44D8F8CFF431BC69ED5587C68F89F0663F2BC1FBBFCB46DC5989
+pow2 = 0x494DBB4E8BD688AA
+digest = generate_pow_hash(pow2, root)
+print(hex(digest))
+print(hex(epoch2_threshold_low))
+print(digest >= epoch2_threshold_low)
