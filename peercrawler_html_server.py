05dc6661a00263fac13bc463539379dc8baf02c6
Dimitrios Siganos <dimitris@siganos.org>
2022-05-08 15:51:42 +0100

    Tidy up peercrawler html server (remove __dict__ code)

--- peercrawler_html_server.py
+++ peercrawler_html_server.py
@@ -20,27 +20,64 @@ def bg_thread_func():
 def main_website():
     global app, peerman
 
-    peers = peerman.get_peers_copy()
-    peers_copy = list(peers)
+    peers_copy = list(peerman.get_peers_copy())
 
     peer_list = []
     for peer in peers_copy:
-        telemetry = peer.__dict__['telemetry']
+        telemetry = peer.telemetry
         hdr = {}
         if telemetry != None:
-            if telemetry.__dict__['hdr'] == None:
-                hdr['ext'] = 0
-                hdr['net_id'] = 0
-                hdr['ver_max'] = 0
-                hdr['ver_using'] = 0
-                hdr['ver_min'] = 0
-                hdr['msg_type'] = 0
+            if telemetry.hdr == None:
+                hdr.ext = 0
+                hdr.net_id = 0
+                hdr.ver_max = 0
+                hdr.ver_using = 0
+                hdr.ver_min = 0
+                hdr.msg_type = 0
             else:
-                hdr = telemetry.__dict__['hdr'].__dict__
+                hdr = telemetry.hdr
 
-            peer_list.append([peer.__dict__['ip'], peer.__dict__['port'], common.hexlify(peer.__dict__['peer_id']), peer.__dict__['is_voting'], hdr['ext'], hdr['net_id'], hdr['ver_max'], hdr['ver_using'], hdr['ver_min'], hdr['msg_type'], telemetry.__dict__['sig_verified'], common.hexlify(telemetry.__dict__['sig']), common.hexlify(telemetry.__dict__['node_id']), telemetry.__dict__['block_count'], telemetry.__dict__['cemented_count'], telemetry.__dict__['unchecked_count'], telemetry.__dict__['account_count'], telemetry.__dict__['bandwidth_cap'], telemetry.__dict__['peer_count'], telemetry.__dict__['protocol_ver'], telemetry.__dict__['uptime'], common.hexlify(telemetry.__dict__['genesis_hash']), telemetry.__dict__['major_ver'], telemetry.__dict__['minor_ver'], telemetry.__dict__['patch_ver'], telemetry.__dict__['pre_release_ver'], telemetry.__dict__['maker_ver'], telemetry.__dict__['timestamp'], telemetry.__dict__['active_difficulty'], peer.__dict__['aux'], peer.__dict__['score']])
+            peer_list.append([peer.ip,
+                              peer.port,
+                              common.hexlify(peer.peer_id),
+                              peer.is_voting,
+                              hdr.ext,
+                              hdr.net_id,
+                              hdr.ver_max,
+                              hdr.ver_using,
+                              hdr.ver_min,
+                              hdr.msg_type,
+                              telemetry.sig_verified,
+                              common.hexlify(telemetry.sig),
+                              common.hexlify(telemetry.node_id),
+                              telemetry.block_count,
+                              telemetry.cemented_count,
+                              telemetry.unchecked_count,
+                              telemetry.account_count,
+                              telemetry.bandwidth_cap,
+                              telemetry.peer_count,
+                              telemetry.protocol_ver,
+                              telemetry.uptime,
+                              common.hexlify(telemetry.genesis_hash),
+                              telemetry.major_ver,
+                              telemetry.minor_ver,
+                              telemetry.patch_ver,
+                              telemetry.pre_release_ver,
+                              telemetry.maker_ver,
+                              telemetry.timestamp,
+                              telemetry.active_difficulty,
+                              peer.aux,
+                              peer.score])
         else:
-            peer_list.append([peer.__dict__['ip'], peer.__dict__['port'], common.hexlify(peer.__dict__['peer_id']), peer.__dict__['is_voting'], "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", peer.__dict__['aux'], peer.__dict__['score']])
+            peer_list.append([peer.ip,
+                              peer.port,
+                              common.hexlify(peer.peer_id),
+                              peer.is_voting,
+                              "0", "0", "0", "0", "0", "0", "0", "0",
+                              "0", "0", "0", "0", "0", "0", "0", "0",
+                              "0", "0", "0", "0", "0", "0", "0", "0",
+                              "0", "0",
+                              peer.aux, peer.score])
 
     return render_template('index.html', name=peer_list)
 
@@ -50,7 +87,7 @@ def main():
 
     # start flash server in the foreground or debug=True cannot be used otherwise
     # flask expects to be in the foreground
-    app.run(port=8888)
+    app.run(host='0.0.0.0', port=8888, debug=True)
 
 
 if __name__ == "__main__":
