f78aeda8e15ae285e1a8a45e02a34d2b9dbaa71d
Miguel <34418635+miguel1117@users.noreply.github.com>
2022-05-23 19:59:34 +0100

    Removed unnecessary columns and added a column with the nano address and its name if known

--- peercrawler_html_server_utils.py
+++ peercrawler_html_server_utils.py
@@ -1,4 +1,5 @@
 from json import loads
+from typing import Any
 
 
 def try_load_peers_details(path: str) -> list:
@@ -13,10 +14,13 @@ def try_load_peers_details(path: str) -> list:
         return []
 
 
-def get_node_details_from_id(node_id: str, node_details: list) -> dict:
+def get_node_details_from_id(node_id: str, node_details: list) -> dict | None:
     for i in node_details:
         try:
-            name = i["node_id"]
+            if i["node_id"] == node_id:
+                return i
 
         except KeyError:
             continue
+
+    return None
