795cb5c1ca75e6a35f3f2d2d533497c930e4f217
Miguel <34418635+miguel1117@users.noreply.github.com>
2022-06-04 21:55:49 +0100

    Add logging messages to incoming connection handler

--- peercrawler.py
+++ peercrawler.py
@@ -92,6 +92,7 @@ class peer_manager:
                 threading.Thread(target=self.handle_incoming, args=(connection, address), daemon=True).start()
 
     def handle_incoming(self, connection: socket.socket, address):
+        self.logger.debug(f"Receiving connection from {address}")
         while True:
             header, payload = get_next_hdr_payload(connection)
 
@@ -102,11 +103,13 @@ class peer_manager:
                 query = handshake_query.parse_query(header, payload)
                 signing_key, verifying_key = node_handshake_id.keypair()
                 handshake_exchange_server(self.ctx, connection, query, signing_key, verifying_key)
+                self.logger.debug(f"Successful handshake from from {address}")
 
             elif header.msg_type == message_type(message_type_enum.keepalive):
                 keepalive = message_keepalive.parse_payload(header, payload)
                 self.peers.update(keepalive.peers)
 
+                self.logger.debug(f"Received peers from {address}, connection is now closing")
                 connection.close()
                 return
 
