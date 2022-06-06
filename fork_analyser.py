782fd37c582fa8b295f5fcbef43abb8f7bfeb4f5
Patryk Prejs <pprejs22@gmail.com>
2022-06-06 15:42:11 +0100

    Added type hints to fork_analyser.py

--- fork_analyser.py
+++ fork_analyser.py
@@ -9,7 +9,7 @@ from pynanocoin import *
 from exceptions import *
 
 
-def frontier_req(ctx, s, peer, acc_id):
+def frontier_req(ctx: dict, s: socket.socket, peer: Peer, acc_id: bytes):
     hdr = frontier_request.frontier_request.generate_header(ctx, True)
     frontier = frontier_request.frontier_request(hdr, start_account=acc_id, maxacc=1)
     s.send(frontier.serialise())
@@ -30,7 +30,7 @@ def frontier_req(ctx, s, peer, acc_id):
         (peer.ip, peer.port, hexlify(peer.aux['confirmed_frontier']), hexlify(peer.aux['unconfirmed_frontier'])))
 
 
-def pull_blocks(ctx, blockman, peer, acc):
+def pull_blocks(ctx: dict, blockman: block_manager, peer: Peer, acc: bytes):
     print('pull blocks for account %s from peer %s' % (hexlify(acc), peer))
     with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
@@ -66,7 +66,7 @@ def pull_blocks(ctx, blockman, peer, acc):
         #    print(b)
 
 
-def once(ctx, workdir, forkacc):
+def once(ctx: dict, workdir: str, forkacc: bytes):
     workdir = '%s/%s' % (workdir, hexlify(forkacc))
     print(workdir)
 
