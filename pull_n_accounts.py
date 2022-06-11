d384514b27ded43223d84b68cdb4d953f07e927a
Patryk Prejs <pprejs22@gmail.com>
2022-06-11 17:45:48 +0100

    Added return type hints to pull_n_accounts.py

--- pull_n_accounts.py
+++ pull_n_accounts.py
@@ -16,7 +16,7 @@ def store_frontiers_handler(frontiers: list[frontier_entry]):
     return add_frontier
 
 
-def valid_account(acc: bytes):
+def valid_account(acc: bytes) -> bool:
     # if acc == b"\x05\x9fh\xaa\xb2\x9d\xe0\xd3\xa2tCb\\~\xa9\xcd\xdbe\x17\xa8\xb7o\xe3w'\xefjMv\x83*\xd5":
     #     return False
     if acc == b'\x00' * 32:
@@ -24,7 +24,7 @@ def valid_account(acc: bytes):
     return True
 
 
-def main():
+def main() -> None:
     ctx = testctx
     s, _ = get_initial_connected_socket(ctx)
     assert s
