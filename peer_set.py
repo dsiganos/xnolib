a294dc3cb1586e2551ce50cc0f2217552c69c680
Miguel <34418635+miguel1117@users.noreply.github.com>
2023-05-21 20:38:06 +0100

    Don't inherit peer_set from set

--- peer_set.py
+++ peer_set.py
@@ -10,7 +10,13 @@ from peer import Peer
 logger = _logger.get_logger()
 
 
-class peer_set(set):
+class peer_set:
+    """
+    Represents a collection of unique peers in a network. This collection is not thread-safe.
+    """
+    def __init__(self):
+        self.__peers: set[Peer] = set()
+
     def add(self, new_peer: Peer) -> None:
         assert isinstance(new_peer, Peer)
 
@@ -18,21 +24,45 @@ class peer_set(set):
         if existing:
             existing.merge(new_peer)
         else:
-            super(peer_set, self).add(new_peer)
+            self.__peers.add(new_peer)
+
+    # def remove(self, element: Peer) -> bool:
+    #     """
+    #     Looks for a peer in this collection that is considered equal to the provided peer and removes it.
+    #     Returns True if a peer was removed, False otherwise..
+    #     """
+    #     assert isinstance(element, Peer)
+    #
+    #     for p in self.__peers:
+    #         if p == element or p.compare(element):
+    #             self.__peers.remove(p)
+    #             return True
+    #
+    #     return False
 
     def update(self, s: Iterable[Peer]) -> None:
         for p in s:
             self.add(p)
 
-    def find(self: set[Peer], element: Peer) -> Optional[Peer]:
-        for p in self:
+    def find(self, element: Peer) -> Optional[Peer]:
+        """
+        Looks for a peer in this collection that is considered equal to the provided peer.
+        Returns a peer or None if none found.
+        """
+        assert isinstance(element, Peer)
+
+        for p in self.__peers:
             if p.compare(element):
                 return p
 
         return None
 
-    def cleanup_inactive(self, inactivity_threshold_seconds: int):
-        for peer in self.copy():
-            if time() - peer.last_seen > inactivity_threshold_seconds:
+    def cleanup_inactive(self, inactivity_threshold_seconds: int) -> None:
+        t = time()
+        for peer in self.__peers.copy():
+            if t - peer.last_seen > inactivity_threshold_seconds:
                 logger.debug(f"Removing inactive peer {peer}")
-                self.remove(peer)
+                try:
+                    self.__peers.remove(peer)
+                except KeyError:
+                    pass
