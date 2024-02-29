5327af4271f2101088ac24918a9085142b30fbef
Dimitrios Siganos <dimitris@siganos.org>
2024-02-29 12:24:23 +0000

    WIP: Handshake v2

--- msg_handshake.py
+++ msg_handshake.py
@@ -19,17 +19,26 @@ class node_handshake_id:
         return ed25519_blake2b.create_keypair()
 
     @classmethod
-    def perform_handshake_exchange(cls, ctx: dict, s: socket.socket, signing_key: ed25519_blake2b.SigningKey,
+    def perform_handshake_exchange(cls, ctx: dict, s: socket.socket,
+                                   signing_key: ed25519_blake2b.SigningKey,
                                    verifying_key: ed25519_blake2b.VerifyingKey) -> bytes:
-        hdr = message_header(ctx['net_id'], [18, 18, 18], message_type(10), 1)
-        msg_handshake = handshake_query(hdr)
+        hdr = message_header(ctx['net_id'], [20, 20, 18], message_type(10), 5)
+        msg_handshake = handshake_query(ctx, hdr)
+        print(msg_handshake)
         s.sendall(msg_handshake.serialise())
         try:
-            data = read_socket(s, 136)
-            hdr = message_header.parse_header(data[0:8])
-            recvd_response = handshake_response_query.parse_query_response(hdr, data[8:])
+            data = read_socket(s, 8)
+            hdr = message_header.parse_header(data)
+            print(hdr)
+            if hdr.is_v2:
+                data = read_socket(s, 128+32+32)
+            else:
+                data = read_socket(s, 128)
+            print('parsing')
+            recvd_response = handshake_response_query.parse_query_response(hdr, data)
 
             response = handshake_response.create_response(ctx, recvd_response.cookie, signing_key, verifying_key)
+            print('sending response')
             s.sendall(response.serialise())
 
             vk = ed25519_blake2b.keys.VerifyingKey(recvd_response.account)
@@ -41,28 +50,42 @@ class node_handshake_id:
 
 
 class handshake_query(node_handshake_id):
-    def __init__(self, hdr: message_header, cookie: bytes = os.urandom(32)):
+    def __init__(self,
+                 ctx: dict,
+                 hdr: message_header,
+                 cookie: bytes = os.urandom(32),
+                 v2: bool = True):
         assert isinstance(hdr, message_header)
         assert hdr.is_query()
         self.header = hdr
         self.cookie = cookie
+        self.v2 = v2
+        self.salt = os.urandom(32)
+        self.genesis = ctx['genesis_block'].hash()
 
     def serialise(self) -> bytes:
         data = self.header.serialise_header()
+        if self.v2:
+            data += self.salt
+            data += self.genesis
         data += self.cookie
         return data
 
     @classmethod
-    def parse_query(cls, hdr: message_header, data: bytes):
+    def parse_query(cls, ctx: dict, hdr: message_header, data: bytes):
         assert(len(data) == 32)
         cookie = data
-        return handshake_query(hdr, cookie)
+        return handshake_query(ctx, hdr, cookie)
 
     def __str__(self):
         string = "Header: [%s]\n" % str(self.header)
         string += "Cookie: %s\n" % hexlify(self.cookie)
-        string += "Is query: True\n"
-        string += "Is response: False\n"
+        string += "Is query: %s\n" % self.header.is_query()
+        string += "Is response: %s\n" % self.header.is_response()
+        string += "Is v2: %s\n" % self.header.is_v2()
+        if self.v2:
+            string += "Salt: %s\n" % hexlify(self.salt)
+            string += "Genesis: %s\n" % hexlify(self.genesis)
         return string
 
     def __eq__(self, other):
@@ -94,7 +117,7 @@ class handshake_response(node_handshake_id):
     def create_response(cls, ctx, cookie, signing_key: ed25519_blake2b.SigningKey,
                         verifying_key: ed25519_blake2b.VerifyingKey):
         sig = signing_key.sign(cookie)
-        hdr = message_header(ctx['net_id'], [18, 18, 18], message_type(10), 2)
+        hdr = message_header(ctx['net_id'], [20, 20, 18], message_type(10), 4+2)
         return handshake_response(hdr, verifying_key.to_bytes(), sig)
 
     @classmethod
@@ -130,7 +153,13 @@ class handshake_response(node_handshake_id):
 
 
 class handshake_response_query(node_handshake_id):
-    def __init__(self, hdr: message_header, cookie: bytes, account: bytes, signature: bytes):
+    def __init__(self,
+                 hdr: message_header,
+                 cookie: bytes,
+                 account: bytes,
+                 salt: bytes,
+                 genesis: bytes,
+                 signature: bytes):
         assert isinstance(hdr, message_header)
         assert hdr.is_query()
         assert hdr.is_response()
@@ -138,27 +167,40 @@ class handshake_response_query(node_handshake_id):
         self.header = hdr
         self.cookie = cookie
         self.account = account
+        self.salt = salt
+        self.genesis = genesis
         self.sig = signature
 
     def serialise(self) -> bytes:
         data = self.header.serialise_header()
         data += self.cookie
         data += self.account
+        data += self.salt
+        data += self.genesis
         data += self.sig
         return data
 
     @classmethod
     def parse_query_response(cls, hdr: message_header, data: bytes):
         assert isinstance(hdr, message_header)
-        assert(len(data) == 128)
+        assert(len(data) == 128 or len(data) == 128+32+32)
 
-        cookie = data[0:32]
-        account = data[32:64]
-        sig = data[64:]
+        if hdr.is_v2:
+            cookie = data[0:32]
+            account = data[32:64]
+            salt = data[64:96]
+            genesis = data[96:128]
+            sig = data[128:]
+        else:
+            cookie = data[0:32]
+            account = data[32:64]
+            sig = data[64:]
 
         assert len(sig) == 64
 
-        return handshake_response_query(hdr, cookie, account, sig)
+        print(hexlify(account))
+        print(hexlify(genesis))
+        return handshake_response_query(hdr, cookie, account, salt, genesis, sig)
 
     @classmethod
     def create_response(cls, ctx, cookie, signing_key: ed25519_blake2b.SigningKey,
